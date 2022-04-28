b05d2d2e07d75fcf76545a374e3d771caa1c508b
Shamik <64892076+shazm12@users.noreply.github.com>
2022-04-28 12:18:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,7 @@ import { getFirestore, setDoc, doc } from 'firebase/firestore';
 import { collection, getDocs,addDoc } from "firebase/firestore"; 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAMRjfhFiRg_Fqw_LHLXqlBaPUfrkDa19g",
-    authDomain: "fir-auth-445fc.firebaseapp.com",
-    projectId: "fir-auth-445fc",
-    storageBucket: "fir-auth-445fc.appspot.com",
-    messagingSenderId: "1055896784620",
-    appId: "1:1055896784620:web:3331246573a4d236759fc9"
+    //Your firebase API credentials
 };
 
 const app = initializeApp(firebaseConfig);
